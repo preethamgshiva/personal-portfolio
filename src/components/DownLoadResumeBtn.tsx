0b550b7404efc35066d875d54ba41b7213e26df5
Preetham G Shiva <preethamgshivva2004@gmail.com>
2025-06-25 23:33:48 +0530

    Add working resume.pdf and update download functionality

--- src/components/DownLoadResumeBtn.tsx
+++ src/components/DownLoadResumeBtn.tsx
@@ -1,21 +1,17 @@
-import { Download } from 'lucide-react'
-import Link from 'next/link'
-import React from 'react'
-import { buttonVariants } from './ui/button'
+import HackerBtn from './animation/HackerBtn'
 
 function DownLoadResumeBtn() {
   return (
     <div className="h-fit w-full mt-2 py-2 px-4">
-      <Link
+      <a
         href="/resume.pdf"
         download
         target="_blank"
         rel="noopener noreferrer"
-        className={buttonVariants({ variant: "default", size: "lg" })}
+        style={{ textDecoration: 'none' }}
       >
-        <Download className="mx-1" />
-        Download Resume
-      </Link>
+        <HackerBtn label="Download Resume" />
+      </a>
     </div>
   )
 }
