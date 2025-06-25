cd200a16f7c5ee709089595eb4ff6238c8d7571f
Preetham G Shiva <preethamgshivva2004@gmail.com>
2025-06-25 22:20:26 +0530

    Initial commit

---
+++ src/components/SocialLinks.tsx
@@ -0,0 +1,59 @@
+import { cn } from "@/lib/utils";
+import {
+  Linkedin,
+  Instagram,
+  Github,
+  Mail,
+} from "lucide-react";
+import { buttonVariants } from "./ui/button";
+import Link from "next/link";
+import FramerWrapper from "./animation/FramerWrapper";
+import { portfolioConfig } from "@/config/portfolio.config";
+
+const SocialLinks = () => {
+  const links = [
+    {
+      name: "Linkedin",
+      link: portfolioConfig.socialLinks.linkedin,
+      icon: <Linkedin />,
+    },
+    {
+      name: "Github",
+      link: portfolioConfig.socialLinks.github,
+      icon: <Github />,
+    },
+    {
+      name: "Instagram",
+      link: portfolioConfig.socialLinks.instagram,
+      icon: <Instagram />,
+    },
+    {
+      name: "Gmail",
+      link: portfolioConfig.socialLinks.gmail,
+      icon: <Mail />,
+    },
+  ];
+  return (
+    <>
+      {links.map((itm, indx) => {
+        const timing = 0.55 + indx * 0.125;
+
+        return (
+          <FramerWrapper key={indx} delay={timing} y={50}>
+            <Link
+              target="blank"
+              href={itm.link}
+              className={cn(
+                buttonVariants({ variant: "outline", size: "icon" })
+              )}
+            >
+              {itm.icon}
+            </Link>
+          </FramerWrapper>
+        );
+      })}
+    </>
+  );
+};
+
+export default SocialLinks;
